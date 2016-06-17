3623d0e4f9ddfda8ab742c2ebacbd82d0033f778
Spencer Park <spinnr95@gmail.com>
2016-06-17 19:15:53 -0400

    Added suport for a super table for SymbolTables that introduces the concept of a variable scope

--- src/main/java/cas/cs4tb3/mellowd/intermediate/variables/SymbolTable.java
+++ src/main/java/cas/cs4tb3/mellowd/intermediate/variables/SymbolTable.java
@@ -1,8 +1,11 @@
 //Symbol Table
 //============
 
-package cas.cs4tb3.mellowd.parser;
+package cas.cs4tb3.mellowd.intermediate.variables;
 
+import cas.cs4tb3.mellowd.parser.IncorrectTypeException;
+import cas.cs4tb3.mellowd.parser.UndefinedReferenceException;
+import com.sun.istack.internal.Nullable;
 import org.antlr.v4.runtime.Token;
 
 import java.util.HashMap;
@@ -14,10 +17,12 @@ import java.util.Map;
 //compiler isn't convinced that it has been checked.
 @SuppressWarnings("unchecked")
 public class SymbolTable {
+    private final SymbolTable superTable;
     //The `declarations` are the actually `name -> value` mappings.
     private Map<String, Object> declarations;
 
-    public SymbolTable() {
+    public SymbolTable(@Nullable SymbolTable superTable) {
+        this.superTable = superTable;
         declarations = new HashMap<>();
     }
 
@@ -35,21 +40,32 @@ public class SymbolTable {
     //value is returned.
     public <T> T getDeclarationValue(String identifier, Class<T> type) {
         Object value = declarations.get(identifier);
+        if (value == null) return superTable == null ? null : superTable.getDeclarationValue(identifier, type);
         return type.isInstance(value) ? (T) value : null;
+    }
+
+    public Object getDeclarationValue(String identifier) {
+        Object value = declarations.get(identifier);
+        if (value == null && superTable != null)
+            return superTable.getDeclarationValue(identifier);
+        return value;
     }
 
     //`getDeclarationValueOrThrow` is similar in function to `getDeclarationValue` but instead
     //of returning null, the appropriate exception will be thrown.
     public <T> T getDeclarationValueOrThrow(Token token, Class<T> type) {
-        Object value = declarations.get(token.getText());
-        //If the value is null then there is nothing defined so throw an
+        Object value = getDeclarationValue(token.getText());
+
+        //If the value is still null then there is nothing defined so throw an
         //[UndefinedReferenceException](UndefinedReferenceException.html).
         if (value == null)
             throw new UndefinedReferenceException(token, "Identifier ("+token.getText()+") is undefined.");
+
         //If the type of the value is incorrect then something is defined but it is the wrong type
         //so throw an [IncorrectTypeException](IncorrectTypeException.html).
         if (!type.isInstance(value))
             throw new IncorrectTypeException(token, "Identifier ("+token.getText()+") points to a "+value.getClass().getSimpleName()+" not a "+type.getSimpleName());
+
         //Otherwise all is fine so we can safely cast and return the value
         return (T) value;
     }
@@ -57,7 +73,7 @@ public class SymbolTable {
     //`getType` is a utility method that will return the type of the data. This
     //will return null if the identifier is not defined.
     public Class<?> getType(String identifier) {
-        Object value = declarations.get(identifier);
+        Object value = getDeclarationValue(identifier);
         return value == null ? null : value.getClass();
     }
 
@@ -65,14 +81,14 @@ public class SymbolTable {
     //will return true if the identifier is defined and points to an object
     //that can safely be cast to `type`
     public boolean identifierTypeIs(String identifier, Class<?> type) {
-        Object value = declarations.get(identifier);
+        Object value = getDeclarationValue(identifier);
         return value != null && type.isAssignableFrom(value.getClass());
     }
 
     //`checkType` is the equivalent of `identifierTypeIs` throwing an exception
     //if the type is defined and incorrect (not able to be cast to `type`).
     public void checkType(Token token, Class<?> type) {
-        Object value = declarations.get(token.getText());
+        Object value = getDeclarationValue(token.getText());
         if (value != null && !type.isAssignableFrom(value.getClass()))
             throw new IncorrectTypeException(token, "Identifier (" + token.getText() + ") points to a " + value.getClass().getSimpleName() + " not a " + type.getSimpleName());
     }
@@ -80,7 +96,7 @@ public class SymbolTable {
     //`checkExists` is a utility method that throws an exception if the identifier that
     //the `token` points to is not defined.
     public void checkExists(Token token) {
-        Object value = declarations.get(token.getText());
+        Object value = getDeclarationValue(token.getText());
         if (value == null)
             throw new UndefinedReferenceException(token, "Identifier ("+token.getText()+") is undefined.");
     }
